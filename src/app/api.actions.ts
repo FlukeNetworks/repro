8d0f7009bf6cbf4e3ad67412150d463d0288f79f
Ardeshir <ardeshir.javaherchi@gmail.com>
2018-05-22 17:06:08 -0700

    form -> api

--- src/app/api.actions.ts
+++ src/app/api.actions.ts
@@ -1,45 +1,45 @@
 import { Action } from '@ngrx/store';
 
-export enum FormActionTypes {
-  LoadAttributes = '[Form] LoadAttributes',
-  PostsLoaded = '[Form] PostsLoaded',
-  CommentsLoaded = '[Form] CommentsLoaded',
-  AlbumsLoaded = '[Form] AlbumsLoaded',
-  PhotosLoaded = '[Form] PhotosLoaded',
-  TodosLoaded = '[Form] TodosLoaded'
+export enum ApiActionTypes {
+  LoadAttributes = '[Api] LoadAttributes',
+  PostsLoaded = '[Api] PostsLoaded',
+  CommentsLoaded = '[Api] CommentsLoaded',
+  AlbumsLoaded = '[Api] AlbumsLoaded',
+  PhotosLoaded = '[Api] PhotosLoaded',
+  TodosLoaded = '[Api] TodosLoaded'
 }
 
 export class LoadAttributesAction implements Action {
-  readonly type = FormActionTypes.LoadAttributes;
+  readonly type = ApiActionTypes.LoadAttributes;
   readonly payload = null;
 }
 
 export class PostsLoadedAction implements Action {
-  readonly type = FormActionTypes.PostsLoaded;
+  readonly type = ApiActionTypes.PostsLoaded;
 
   constructor(public payload: any) {}
 }
 
 export class CommentsLoadedAction implements Action {
-  readonly type = FormActionTypes.CommentsLoaded;
+  readonly type = ApiActionTypes.CommentsLoaded;
 
   constructor(public payload: any) {}
 }
 
 export class AlbumsLoadedAction implements Action {
-  readonly type = FormActionTypes.AlbumsLoaded;
+  readonly type = ApiActionTypes.AlbumsLoaded;
 
   constructor(public payload: any) {}
 }
 
 export class PhotosLoadedAction implements Action {
-  readonly type = FormActionTypes.PhotosLoaded;
+  readonly type = ApiActionTypes.PhotosLoaded;
 
   constructor(public payload: any) {}
 }
 
 export class TodosLoadedAction implements Action {
-  readonly type = FormActionTypes.TodosLoaded;
+  readonly type = ApiActionTypes.TodosLoaded;
 
   constructor(public payload: any) {}
 }
