451912b3605db263759611eaccb1f6b6dbf74a54
Ardeshir <ardeshir.javaherchi@gmail.com>
2018-05-22 17:10:13 -0700

    Add LoadComments LoadAlbums LoadTodos effects

--- src/app/app.effects.ts
+++ src/app/app.effects.ts
@@ -22,4 +22,34 @@ export class AppEffects {
         .map(posts => new ApiActions.PostsLoadedAction(posts))
     })
   );
+
+  @Effect()
+  LoadComments = this.actions$.pipe(
+    ofType(ApiActions.ApiActionTypes.LoadAttributes),
+    flatMap(action => {
+      return this._apiService
+        .loadComments()
+        .map(comments => new ApiActions.CommentsLoadedAction(comments))
+    })
+  );
+
+  @Effect()
+  LoadAlbums = this.actions$.pipe(
+    ofType(ApiActions.ApiActionTypes.LoadAttributes),
+    flatMap(action => {
+      return this._apiService
+        .loadAlbums()
+        .map(comments => new ApiActions.AlbumsLoadedAction(comments))
+    })
+  );
+
+  @Effect()
+  LoadTodos = this.actions$.pipe(
+    ofType(ApiActions.ApiActionTypes.LoadAttributes),
+    flatMap(action => {
+      return this._apiService
+        .loadTodos()
+        .map(todos => new ApiActions.TodosLoadedAction(todos))
+    })
+  );
 }
