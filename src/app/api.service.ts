47587d1bdfad929a6515d86b99612644ecdd4bdf
Allan Ortiz <aortez@gmail.com>
2018-06-06 11:20:33 -0700

    remove external API dependency

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -8,9 +9,16 @@ export class ApiService {
   constructor(private _http: HttpClient) { }
 
   loadPhotos() {
-    return this._http
-      .get("https://jsonplaceholder.typicode.com/photos")
-      .map(response => response);
+    // const photosObservable = this._http.get("https://jsonplaceholder.typicode.com/photos");
+
+    const photos = [];
+    for (let i = 0; i < 5000; i++ ) {
+      photos.push({ title: i });
+    }
+    const photosObservable = Observable.create(observer => { observer.next(photos); });
+
+    // console.log(`photosObservable: ${JSON.stringify(photosObservable)}`);
+    return photosObservable;
   }
 
 }
