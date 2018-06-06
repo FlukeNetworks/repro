a869d88e1feef4ec745f004d7d704648805f358a
Allan Ortiz <aortez@gmail.com>
2018-06-06 13:09:51 -0700

    remove async pipe and store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,21 @@
 import { Component } from '@angular/core';
-import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs/Observable';
-
-import { State } from "./reducers";
-import * as ApiActions from "./api.actions";
-import * as fromRoot from "./reducers";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
 
-  photos: Observable<any>;
+  photos: any;
 
-  constructor(private _store: Store<State>) {
-    this.photos = this._store.select(fromRoot.getPhotos);
+  constructor() {
   }
 
   OnClick() {
-    this._store.dispatch(new ApiActions.LoadAttributesAction());
+    const photos = [];
+    for (let i = 0; i < 5000; i++) {
+      photos.push({ title: i });
+    }
+    this.photos = photos;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
-import { EffectsModule } from '@ngrx/effects';
-import { AppEffects } from './app.effects';
-import { ApiService } from './api.service';
 
 
 @NgModule({
@@ -17,13 +9,8 @@ import { ApiService } from './api.service';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    StoreModule.forRoot(reducers, { metaReducers }),
-    !environment.production ? StoreDevtoolsModule.instrument() : [],
-    EffectsModule.forRoot([AppEffects]),
-    HttpClientModule
+    BrowserModule
   ],
-  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
